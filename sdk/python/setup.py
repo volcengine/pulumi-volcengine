bc6b12fffcda3e5f3549fe8e15f8f0935ef06ee5
songzheng <songzheng.sz@bytedance.com>
2023-07-31 16:37:58 +0800

    Support Python and Node SDK install.

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -15,7 +15,7 @@ class InstallPluginCommand(install):
     def run(self):
         install.run(self)
         try:
-            check_call(['pulumi', 'plugin', 'install', 'resource', 'volcengine', PLUGIN_VERSION])
+            check_call(['pulumi', 'plugin', 'install', 'resource', 'volcengine', PLUGIN_VERSION], '--server', 'github://api.github.com/volcengine')
         except OSError as error:
             if error.errno == errno.ENOENT:
                 print(f"""
